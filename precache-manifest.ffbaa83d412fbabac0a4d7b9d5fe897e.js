a9e69c2cc53f7cca9c1dfe24716490b36900127c
Xander <GreatXander1@hotmail.com>
2020-10-10 14:36:57 -0400

    Updates

---
+++ precache-manifest.ffbaa83d412fbabac0a4d7b9d5fe897e.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2b7c69b3075e6148334baa3f54b334e",
+    "url": "/ITM-PROJECT/index.html"
+  },
+  {
+    "revision": "252a3395c790db85feb3",
+    "url": "/ITM-PROJECT/static/css/main.1847cc0e.chunk.css"
+  },
+  {
+    "revision": "559c92916019aa97edca",
+    "url": "/ITM-PROJECT/static/js/2.f0257b94.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ITM-PROJECT/static/js/2.f0257b94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "252a3395c790db85feb3",
+    "url": "/ITM-PROJECT/static/js/main.be27de89.chunk.js"
+  },
+  {
+    "revision": "ea75fef038370aa04d8a",
+    "url": "/ITM-PROJECT/static/js/runtime-main.49d206b4.js"
+  },
+  {
+    "revision": "b8673c57ebdfa860b117dd38f82f2790",
+    "url": "/ITM-PROJECT/static/media/backpack.b8673c57.svg"
+  },
+  {
+    "revision": "55b2012bbd0dbfc13fd62cec540f1e49",
+    "url": "/ITM-PROJECT/static/media/backpack__contrast.55b2012b.svg"
+  },
+  {
+    "revision": "d92cf23de1fb89760bc7c2bfdc5faccf",
+    "url": "/ITM-PROJECT/static/media/ball.d92cf23d.svg"
+  },
+  {
+    "revision": "730921c440b0654b01b81b3e4eeab98d",
+    "url": "/ITM-PROJECT/static/media/book__blue.730921c4.svg"
+  },
+  {
+    "revision": "e588f368e993345ab7f3fe146a0783e8",
+    "url": "/ITM-PROJECT/static/media/book__red.e588f368.svg"
+  },
+  {
+    "revision": "a342f0c0a4788ca54ec74eeb6b9cc664",
+    "url": "/ITM-PROJECT/static/media/check.a342f0c0.svg"
+  },
+  {
+    "revision": "aca518ca7c6ec043686fd498e637660b",
+    "url": "/ITM-PROJECT/static/media/community.aca518ca.svg"
+  },
+  {
+    "revision": "e4b8e24b66b5a8b0fb2a9054138f03c6",
+    "url": "/ITM-PROJECT/static/media/community__logo.e4b8e24b.svg"
+  },
+  {
+    "revision": "7abd2f573df424c78c62d4414b6e15c1",
+    "url": "/ITM-PROJECT/static/media/community__tag.7abd2f57.svg"
+  },
+  {
+    "revision": "40767fd5c4333e47f4436ab9e42bb540",
+    "url": "/ITM-PROJECT/static/media/director.40767fd5.svg"
+  },
+  {
+    "revision": "b0b19cf61fb87ad58b05f57f34178384",
+    "url": "/ITM-PROJECT/static/media/director__contrast.b0b19cf6.svg"
+  },
+  {
+    "revision": "cb04b4ff9939c1e6045e2a807b4bafbb",
+    "url": "/ITM-PROJECT/static/media/eraser.cb04b4ff.svg"
+  },
+  {
+    "revision": "caeede80608b9cb2887ba0a55d707a23",
+    "url": "/ITM-PROJECT/static/media/error.caeede80.svg"
+  },
+  {
+    "revision": "818587a2d1852e3cd17b1721fe006f47",
+    "url": "/ITM-PROJECT/static/media/example.818587a2.png"
+  },
+  {
+    "revision": "4f65b46d3e88811296845edfa118616b",
+    "url": "/ITM-PROJECT/static/media/glasses.4f65b46d.svg"
+  },
+  {
+    "revision": "c2de2c02b6ebc783e936612f8eaccc53",
+    "url": "/ITM-PROJECT/static/media/home__contrast.c2de2c02.svg"
+  },
+  {
+    "revision": "6aa71dc95efa672398513251d6599725",
+    "url": "/ITM-PROJECT/static/media/info__tag.6aa71dc9.svg"
+  },
+  {
+    "revision": "bba97b98d32adfd83fb12d3da75e25f2",
+    "url": "/ITM-PROJECT/static/media/institute__tag.bba97b98.svg"
+  },
+  {
+    "revision": "6d4773096d65b04c2dfe51f2d3e45dd4",
+    "url": "/ITM-PROJECT/static/media/library__tag.6d477309.svg"
+  },
+  {
+    "revision": "e5f684a84a99b118e9b35f70a5859ad3",
+    "url": "/ITM-PROJECT/static/media/menu__button.e5f684a8.svg"
+  },
+  {
+    "revision": "d25f1a9e46c3861eb42388b69c75a267",
+    "url": "/ITM-PROJECT/static/media/note.d25f1a9e.svg"
+  },
+  {
+    "revision": "8b784e5566f10fd8c4e721b8df7ce51a",
+    "url": "/ITM-PROJECT/static/media/offer__tag.8b784e55.svg"
+  },
+  {
+    "revision": "698ada4fb562a938718f614888382a21",
+    "url": "/ITM-PROJECT/static/media/padlock.698ada4f.svg"
+  },
+  {
+    "revision": "118c1c59093d15621cca335127ad5384",
+    "url": "/ITM-PROJECT/static/media/pencil.118c1c59.svg"
+  },
+  {
+    "revision": "61346a493f75604d1dfd0a98eb3af326",
+    "url": "/ITM-PROJECT/static/media/pencil__contrast.61346a49.svg"
+  },
+  {
+    "revision": "052f311c230ad1b54b178399167327f0",
+    "url": "/ITM-PROJECT/static/media/rule.052f311c.svg"
+  },
+  {
+    "revision": "7580ca5bc0d44dd72c213b575b11a9de",
+    "url": "/ITM-PROJECT/static/media/school__draw.7580ca5b.svg"
+  },
+  {
+    "revision": "27d139fe4a810710c1715423d631cc29",
+    "url": "/ITM-PROJECT/static/media/school__logo.27d139fe.svg"
+  },
+  {
+    "revision": "76c1f13134a2beb63430b88d96099d74",
+    "url": "/ITM-PROJECT/static/media/teachers__tag.76c1f131.svg"
+  },
+  {
+    "revision": "3d8edb2816b5b8b4fd3b0af21e82b43e",
+    "url": "/ITM-PROJECT/static/media/us__tag.3d8edb28.svg"
+  },
+  {
+    "revision": "db96e0971ed8150f58fd338bc42e786c",
+    "url": "/ITM-PROJECT/static/media/welcome__tag.db96e097.svg"
+  }
+]);
